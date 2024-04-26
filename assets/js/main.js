5a73a14d1a34d2a58d9464b2aaff31c4d1699700
Sedar Yildirim <155425285+SedarYildirim@users.noreply.github.com>
2024-04-26 18:55:36 +0700

    main content done, breakpoints to do

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,30 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById('header')
+  this.scrollY >= 50 ? header.classList.add('scroll-header')
+                     : header.classList.remove('scroll-header')
+}
 
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER PRODUCTS ===============*/
+
+let swiperProducts = new Swiper(".products__container", {
+spaceBetween: 32,
+grabCursor: true,
+centeredSlides: true,
+slidesPerView: "auto",
+loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 72,
+    },
+  },
+});
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
